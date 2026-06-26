8b1ab51b8cd8e4f667511def856d0b07ac2c78e1
Joownlok <joao.6564693@gmail.com>
2026-06-26 09:18:13 -0300

    Bibliotecas

---
+++ python/bibliotecas/exercicio.py
@@ -0,0 +1,5 @@
+notas = [9, 7, 7, 10, 3, 9, 6, 6, 2]
+
+print(notas.count(7)) #Count é uma função de lista que conta a quantidade de número x
+notas[-1] = 4
+print(notas)
---
+++ python/bibliotecas/importando_bibliotecas.py
@@ -0,0 +1,6 @@
+# Importar uma biblioteca padrão do Python traz mais recursos
+# Python.org -> Docs -> Library 
+import math as m #Importei a biblioteca com resolução matemática, (as) eu dou um apelido para a biblioteca: m
+print(m.sqrt(9))
+
+from math import sqrt #Importando apenas 1 função da biblioteca
